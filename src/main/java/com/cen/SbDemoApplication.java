335116e65fb49533f2bb1222a3d253aebd765693
顺子 <cenjiashun@zhuojianchina.com>
2018-07-30 15:02:16 +0800

    tttt

---
+++ src/main/java/com/cen/SbDemoApplication.java
@@ -0,0 +1,20 @@
+package com.cen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "Welcome to know Spring Boot !";
+    }
+}
